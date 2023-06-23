ca76afe9ea410167ce9a6a431fe0778c218495b9
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-06-23 08:52:02 +0000

    todo's list using useState hook twice, second time with an empty array that we will be updated with the setImputValue from first hook called in the onChange, here we are returning the info in form of  a list, using ul and li. Used fliter to erase the inputs that we can create

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [inputValue, setinputValue] = useState ("")
+	const [toDos, settoDos] = useState ([])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My to do's</h1>
+			<ul>
+				<li>
+					<input 
+					type="text" 
+					onChange= {(e) => setinputValue(e.target.value)}
+					value={inputValue}
+					onKeyPress= {(e) => {
+						if (e.key === "Enter") {
+							settoDos (toDos.concat (inputValue));
+							setinputValue (" ");
+						}
+					 	
+					}}
+					
+					placeholder="Add a task that needs to be done">
+					</input>
+					</li>
+
+				{toDos.map ((task,index) => (
+				<li className="taskAdded">{task}
+				<span className="trashIcon">
+					<i  class="fas fa-trash-alt"  
+					onClick = {() => 
+						settoDos (
+							toDos.filter (
+								(task,currentIndex) => index != currentIndex))}></i>
+				</span>
+				</li>
+				))}
+				
+			</ul>
+			<div className="numberofTasks">{toDos.length} tasks</div>
 		</div>
+		
 	);
 };
 
